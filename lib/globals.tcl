15421b92213ed306652b7cf4ae1dfd8c6fcfb9b5
Andreas Grapentin <andreas@grapentin.org>
2015-04-15 14:26:53 +1200

    updated copyright years

--- lib/globals.tcl
+++ lib/globals.tcl
@@ -2,7 +2,7 @@
  ##############################################################################
  #                                asparagus                                   #
  #                                                                            #
- #    Copyright (C) 2014  Andreas Grapentin                                   #
+ #    Copyright (C) 2014 - 2015  Andreas Grapentin                            #
  #                                                                            #
  #    This program is free software: you can redistribute it and/or modify    #
  #    it under the terms of the GNU General Public License as published by    #
